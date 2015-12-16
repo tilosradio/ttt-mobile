b13ed44319894496c027e09707e17c6a6f377b14
Elek, Márton <elek@users.noreply.github.com>
2015-12-17 00:14:08 +0100

    new: refreshed database with location images

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,24 +1,24 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  .controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
 
-  // Perform the login action when the user submits the login form
+    // Perform the login action when the user submits the login form
 
-})
+  })
 
-.controller('BusinessListCtrl', function($scope, pouchService, position, $rootScope) {
+  .controller('BusinessListCtrl', function ($scope, pouchService, position, $rootScope) {
     pouchService.allDocs({
       include_docs: true,
       attachments: true
     }).then(function (result) {
-      $scope.$apply(function(){
+      $scope.$apply(function () {
         $scope.items = result.rows;
 
       });
@@ -26,20 +26,25 @@ angular.module('starter.controllers', [])
       console.log(err);
     });
 
-    $rootScope.$on('position', function(){
-    angular.forEach($scope.items, function(value, key) {
-             value.doc.distance = position.distance(value.doc.lat, value.doc.long);
-           });
+    $rootScope.$on('position', function () {
+      angular.forEach($scope.items, function (value, key) {
+        value.doc.distance = position.distance(value.doc.lat, value.doc.long);
+      });
     });
-})
+  })
 
-.controller('BusinessCtrl', function($scope, $stateParams, pouchService) {
-    pouchService.get($stateParams.id).then(function (result) {
-      $scope.$apply(function(){
+  .controller('BusinessCtrl', function ($scope, $stateParams, pouchService) {
+    pouchService.get($stateParams.id, {attachment: true}).then(function (result) {
+      $scope.$apply(function () {
         $scope.business = result;
+      });
+      return pouchService.getAttachment($stateParams.id, "photo.jpg");
+    }).then(function (blob) {
+      $scope.$apply(function () {
+        $scope.imgUrl = URL.createObjectURL(blob);
       });
     }).catch(function (err) {
       console.log(err);
     });
-})
+  })
 
